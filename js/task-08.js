36288b844afebeda5a7798edc01686074a15f07f
Roman-Solovei <romario2409@gmail.com>
2021-11-02 20:19:38 +0200

    hw 07 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,30 +1,21 @@
-var render = document.querySelector('[data-action="render"]');
-var destroy = document.querySelector('[data-action="destroy"]');
-var boxes = document.getElementById('boxes');
-render.addEventListener('click', getAmount);
-destroy.addEventListener('click', destroyBoxes);
 
-function getAmount() {
-  var amount = +document.querySelector('#controls input').value;
-  createBoxes(amount);
-}
+const form = document.querySelector(".login-form");
 
-function createBoxes(amount) {
-  var basicSize = 30;
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < amount; i++) {
-    var size = basicSize + i * 10;
-    var div = document.createElement('div');
-    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: rgba( ${random()} , ${random()} , ${random()} )`;
-    fragment.appendChild(div);
+form.addEventListener("submit", handleSubmit);
+
+
+function handleSubmit(event) {
+
+  event.preventDefault();
+
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
   }
-  boxes.appendChild(fragment);
-}
 
-function destroyBoxes() {
-  boxes.innerHTML = '';
-}
-
-function random() {
-  return Math.floor(Math.random() * 256);
+  console.log(`E-mail: ${email.value}, Password: ${password.value}`); 
+   
 }
